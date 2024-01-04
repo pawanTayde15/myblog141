f220352168e3c59354bd026db1737f3af51597ad
Pawan Vijay Tayde <pvt1504@gmail.com>
2024-01-04 14:36:29 +0530

    127:Create save Post Feature Entity class added

---
+++ src/main/java/com/myblog/myblog141/controller/Post.java
@@ -0,0 +1,21 @@
+package com.myblog.myblog141.controller;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "posts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+}
